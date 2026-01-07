ac3562c4f8d7c596811f1d5941d23ca1eb2eed8b
Joywatchin <67507794+Joywatchin@users.noreply.github.com>
2026-01-07 18:34:34 +0800

    build: initial setup for tests - will write tests soon

---
+++ src/test/java/code/backend/CountdownFolderTests.java
@@ -0,0 +1,3 @@
+package code.backend;
+
+public class CountdownFolderTests {}
---
+++ src/test/java/code/backend/CountdownTests.java
@@ -0,0 +1,9 @@
+package code.backend;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class CountdownTests {}

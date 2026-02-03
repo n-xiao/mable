b51a15747ca2f8ba2c47a4c1af8cc15d9465e880
N. Xiao <67507794+n-xiao@users.noreply.github.com>
2026-02-04 00:16:07 +0800

    added comments and null checks

--- src/main/java/code/frontend/libs/katlaf/dragndrop/DragDropOverlay.java
+++ src/main/java/code/frontend/libs/katlaf/dragndrop/DragDropOverlay.java
@@ -9,15 +9,24 @@ import javafx.scene.layout.Region;
  * instances.
  */
 public class DragDropOverlay extends Region {
-    private static DragDropOverlay activeOverlay = null;
+    private static DragDropOverlay activeOverlay = null; // max of one instance should exist
+    private final DragStarter<?> dragStarter;
 
     public DragDropOverlay(DragStarter<?> dragStarter) {
         DragDropOverlay.activeOverlay = this;
         this.dragStarter = dragStarter;
     }
 
-    private final DragStarter<?> dragStarter;
+    /**
+     * A method which compares the class of the data that is being
+     * transferred by the current drag and drop process with a provided
+     * class.
+     *
+     * @return true if the class of the data is equal to the provided class.
+     */
     protected static <E> boolean checkMatchingTypes(Class<? extends E> c) {
+        if (activeOverlay.dragStarter == null || activeOverlay.dragStarter.getData() == null)
+            return false; // if null checks fail
         return activeOverlay.dragStarter.getData().getClass().equals(c);
     }
 }
--- src/main/java/code/frontend/libs/katlaf/dragndrop/DragStopper.java
+++ src/main/java/code/frontend/libs/katlaf/dragndrop/DragStopper.java
@@ -1,10 +1,46 @@
 package code.frontend.libs.katlaf.dragndrop;
 
 public interface DragStopper {
+    /**
+     * This method is called when the mouse is released
+     * whilst hovering over this {@link DragStopper},
+     * finishing the drag process. This method is
+     * a convenience method, and could be implemented
+     * to do nothing.
+     */
     void onDragStop();
-    void onDragHover();
+
+    /**
+     * This method is called whenever the mouse, while
+     * dragging with an active {@link DragDropOverlay},
+     * enters this {@link DragStopper}. This is a
+     * convenience method which can be used to
+     * implement special highlighting or styling to
+     * provide visual indications.
+     *
+     * Tip: you an use the isAccepting() method to check
+     * if the current drag and drop process is transferring
+     * data of relevance to this {@link DragStopper}.
+     */
+    void onDragEnter();
+
+    void onDragExit();
+
+    /**
+     * It is the implementor's responsibility to specify
+     * the expected type of this {@link DragStopper}.
+     * Do so by implementing this method.
+     * This should never return null. Never, ever, ever.
+     */
     Class<?> getExpectedType();
 
+    /**
+     * A check to see if this {@link DragStopper} can accept
+     * the data that is currently being dragged.
+     *
+     * @return true if the data that is being dragged is of the
+     * expected type of this {@link DragStopper}, false otherwise.
+     */
     default boolean isAccepting() {
         return DragDropOverlay.checkMatchingTypes(getExpectedType());
     }
